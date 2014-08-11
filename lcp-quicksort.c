1efaa90c59582f5b297193bff818fc93d4e347c1
Kendall Willets <kendall@Kendalls-MacBook-Air.local>
2014-08-11 09:14:46 -0700

    code cleanup, moved comparison out of loop.

--- lcp-quicksort.c
+++ lcp-quicksort.c
@@ -1,6 +1,5 @@
-#include <stdio.h>
 #include <stdlib.h>
-typedef char * Item;
+#include <lcp-quicksort.h>
 
 int lcpstrcmp( char *p, char *q, int *lcp) {
   char *pi=p+*lcp, *qi=q+*lcp;
@@ -16,53 +15,27 @@ void  exch( Item a[], int lcp[], int I, int J)
 { Item  __tmp = a[I]; a[I]=a[J]; a[J]=__tmp;
   int  __itmp = lcp[I]; lcp[I]=lcp[J], lcp[J]=__itmp; } 
 
-// check lcp's are partitioned in opposite order of direction
-// longer lcp, positive direction => lt
-// etc.
-void check_partition( int lcp[], int lo, int hi, int lt, int gt, int direction, int v ) {
-  // a[lo..lt-1] < v = a[lt..gt] < a[gt+1..hi]
-  int i;
-  for(  i = lo; i <= hi; i++ ) {
-    if( i < lt && -direction*lcp[i] >= -direction*v ) {
-      printf("smaller elt %d  is >= v\n", i);
-      break;
-    } else if( i >= lt && i <= gt && lcp[i] != v ) {
-      printf("equal elt %d is != v\n", i);
-      break;
-    } else if( i > gt && -direction*lcp[i] <= -direction*v ) {
-      printf("larger elt %d is <= v\n",i);
-      break;
-    }
-  }
-  if( i <= hi ) {
-    printf("i = %d, lcp[i] = %d, lo = %d, lt = %d, gt = %d, hi = %d\n", i, lcp[i], lo, lt, gt, hi );
-    for( i=lo; printf(","), i<=hi; i++ ) {
-      printf("%d",lcp[i]);
-    }
-    printf("\n");
-  }
-}
-
 void lcpquicksort( Item a[], int lcp[], int lo, int hi, int direction ) {
   if (hi <= lo) return;
   int lt = lo, gt = hi;
   int v = lcp[lo];
   int i = lo + 1;
-  while (i <= gt)
-    {
+  if( direction < 0 ) {  // direction of old pivot determines effect of lcp > or < pivot
+    while (i <= gt) {
       int t = lcp[i];
-      if( direction < 0 ) {  // direction of old pivot determines effect of lcp > or < pivot
-	if      (t < v) exch(a, lcp, lt++, i++);
-	else if (t > v) exch(a, lcp, i, gt--);
-	else            i++;
-      } else { // old pivot was < a[lo:hi]
-	if      (t > v) exch(a, lcp, lt++, i++); // longer prefix ==> precedes pivot
-	else if (t < v) exch(a, lcp, i, gt--);
-	else            i++;
-      }
+      if      (t < v) exch(a, lcp, lt++, i++);
+      else if (t > v) exch(a, lcp, i, gt--);
+      else            i++;
     }
+  } else {
+    while (i <= gt) { // old pivot was < a[lo:hi]
+      int t = lcp[i];
+      if      (t > v) exch(a, lcp, lt++, i++); // longer prefix ==> precedes pivot
+      else if (t < v) exch(a, lcp, i, gt--);
+      else            i++;
+    }
+  }
   // a[lo..lt-1] < v = a[lt..gt] < a[gt+1..hi]
-  // debug:  check_partition( lcp, lo, hi, lt, gt, direction, v );
   lcpquicksort( a, lcp, lo, lt-1, direction );
   lcpquicksort( a, lcp, gt+1, hi, direction );
 
@@ -86,4 +59,3 @@ void stringsort( Item a[], int n ) {
   lcpquicksort( a, lcp, 0, n-1, 1 );
   free(lcp);
 }
-
