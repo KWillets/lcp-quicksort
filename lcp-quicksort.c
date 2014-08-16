989f45993c5c34a3e7ddfabdbb520013a49fba36
Kendall Willets <kendall@Kendalls-MacBook-Air.local>
2014-08-16 13:25:48 -0700

    fix median bug: not swapping median to lo

--- lcp-quicksort.c
+++ lcp-quicksort.c
@@ -17,15 +17,15 @@ void  exch( Item a[], Lcp lcp[], int I, int J)
   Item  __tmp = a[I]; a[I]=a[J]; a[J]=__tmp;
   Lcp  __itmp = lcp[I]; lcp[I]=lcp[J], lcp[J]=__itmp; } 
 
-int med3func(Lcp va, Lcp vb, Lcp vc)
+int med3func( Lcp l[], int a, int b, int c)
 {  
-  if (va == vb)
-    return va;
-  if (vc == va || vc == vb)
-    return vc;  
-  return va < vb ?
-    (vb < vc ? vb : (va < vc ? vc : va ) )
-    : (vb > vc ? vb : (va < vc ? va : vc ) );
+  if (l[a] == l[b])
+    return a;
+  if (l[c] == l[a] || l[c] == l[b])
+    return c;  
+  return l[a] < l[b] ?
+    (l[b] < l[c] ? b : (l[a] < l[c] ? c : a ) )
+    : (l[b] > l[c] ? b : (l[a] < l[c] ? a : c ) );
 }
 
 void lcpinsertionsort( Item a[], Lcp lcp[], int lo, int hi, int direction) {
@@ -53,24 +53,40 @@ void lcpquicksort( Item a[], Lcp lcp[], int lo, int hi, int direction ) {
   int lt = lo, gt = hi;
   int n = hi - lo + 1;
   int v;
-  v = n > 1000  ? med3func( lcp[lo], lcp[lo+n/2], lcp[hi] ) :
-    lcp[lo];
+  if( n > 1000 ) exch( a, lcp, lo,  med3func( lcp, lo, lo+n/2, hi ));
 
+  v=lcp[lo];
   int i = lo + 1;
   if( direction < 0 ) {  // direction of old pivot determines effect of lcp > or < pivot
-    while (i <= gt) {
+    /*    i=lo;
+    gt = hi;
+    while( i < gt ) {  // two-color DNF 
+      while( lcp[gt] > v && i < gt ) gt--;
+      while( lcp[i] <= v && i < gt ) i++;
+      if( i < gt ) { exch( a, lcp, i, gt ); gt--; i++; };
+    }
+    lt=lo;
+    i=gt;
+    while( i > lt ) {
+      while( lcp[lt] < v && i > lt ) lt++;
+      while( lcp[i] == v && i > lt ) i--;
+      if( i > lt ) { exch( a, lcp, i, lt ); lt++; i--; };
+    }
+    */
+        while (i <= gt) {
       int t = lcp[i];
       if      (t < v) exch(a, lcp, lt++, i++);
       else if (t > v) exch(a, lcp, i, gt--);
       else            i++;
-    }
+      }
+    
   } else {
-    while (i <= gt) { // old pivot was < a[lo:hi]
+      while (i <= gt) { // old pivot was < a[lo:hi]
       int t = lcp[i];
       if      (t > v) exch(a, lcp, lt++, i++); // longer prefix ==> precedes pivot
       else if (t < v) exch(a, lcp, i, gt--);
       else            i++;
-    }
+    }    
   }
   // a[lo..lt-1] < v = a[lt..gt] < a[gt+1..hi]
   lcpquicksort( a, lcp, lo, lt-1, direction );
